4e9fc6d270cd10db9eddb97171c094e257f678e4
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2015-01-09 14:47:56 +0100

    add ZF2/Navigation to timeline

--- module/Timeline/config/module.config.php
+++ module/Timeline/config/module.config.php
@@ -34,4 +34,34 @@ return array(
             'timeline' => __DIR__ . '/../view',
         ),
     ),
+	'navigation' => array(
+		'default' => array(
+			array(
+				'label' => 'Timeline',
+				'route' => 'timeline',
+				'pages' => array(
+					array(
+						'label' => 'Index',
+						'route' => 'timeline',
+						'action' => 'index',
+					),
+					array(
+						'label' => 'Add',
+						'route' => 'timeline',
+						'action'	=> 'add',
+					),
+					array(
+						'label' => 'Edit',
+						'route' => 'timeline',
+						'action' => 'edit',
+					),
+					array(
+						'label' => 'Delete',
+						'route' => 'timeline',
+						'action' => 'delete',
+					),
+				),
+			),
+		),
+	),
 );
