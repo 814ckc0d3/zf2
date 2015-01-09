abaec6b4e30490721fbf73c195f538ac176d39d2
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2015-01-09 14:47:07 +0100

    add ZF2/Navigation to contact config

--- module/Contact/config/module.config.php
+++ module/Contact/config/module.config.php
@@ -38,4 +38,34 @@ return array(
             'contact' => __DIR__ . '/../view',
         ),
     ),
+	'navigation' => array(
+		'default' => array(
+			array(
+				'label' => 'Contact',
+				'route' => 'contact',
+				'pages' => array(
+					array(
+						'label' => 'Index',
+						'route' => 'contact',
+						'action' => 'index',
+					),
+					array(
+						'label' => 'Add',
+						'route' => 'contact',
+						'action'	=> 'add',
+					),
+					array(
+						'label' => 'Edit',
+						'route' => 'contact',
+						'action' => 'edit',
+					),
+					array(
+						'label' => 'Delete',
+						'route' => 'contact',
+						'action' => 'delete',
+					),
+				),
+			),
+		),
+	),
 );
