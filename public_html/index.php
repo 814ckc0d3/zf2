a19a8bc101252351e98736d32264b2ea7f010151
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2020-06-24 20:21:35 +0200

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,17 +6,19 @@
   * Display all errors when APPLICATION_ENV is development.
   */
 
+/** unblock
 if ($_SERVER['APPLICATION_ENV'] == 'development') {
 	error_reporting(E_ALL);
     ini_set("display_errors", 1);
  }else{
  	define('WEBMODE', "visibility:hidden");
  }
+ */
  /**
   * This makes our life easier when dealing with paths. Everything is relative
   * to the application root now.
   */
- chdir(dirname(__DIR__));
+ /** unblock  chdir(dirname(__DIR__));
 
  // Decline static file requests back to the PHP built-in webserver
  if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
@@ -28,3 +30,5 @@ if ($_SERVER['APPLICATION_ENV'] == 'development') {
 
  // Run the application!
  Zend\Mvc\Application::init(require 'config/application.config.php')->run(); 
+ */
+echo 'Hola';
